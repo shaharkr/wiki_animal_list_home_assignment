c47e50f26c72a86c1b803f87390a40b9ecdf2fb8
shaharkr <shahar.kramer@gmail.com>
2023-11-14 21:00:42 +0200

    add src img to animal

--- animal.py
+++ animal.py
@@ -1,6 +1,5 @@
 from configurator import Configurator
 from typing import List
-from dal.web_exetractor import WebExtractor
 import os
 
 configurator = Configurator()
@@ -17,17 +16,28 @@ class Animal:
         self.wiki_page_url = Animal.WIKI_PREFIX_URL.format(link_extension=link_extension)
         self.img_file_name = f"{self.name.replace(' ', '_')}.jpg"
         self.img_path = os.path.join(Animal.IMG_DIR_PATH, self.img_file_name)
+        self.img_src = None
     
     def __repr__(self) -> str:
         return f"Name: {self.name}\n\t\tImage Path: {self.img_path}"
     
+
     def get_wiki_page_url(self):
         return self.wiki_page_url
     
     def get_img_path(self):
         return self.img_path
     
+    def get_name(self):
+        return self.name
+    
+    def set_img_src(self, img_src):
+        self.img_src = img_src
+    
+    def get_img_src(self):
+        return self.img_src
+    
     def save_img(self, img):
         with open(self.img_path, 'wb') as image_file:
             image_file.write(img)
-            print(f"animal {self.name}- Image saved successfully")
+            # print(f"animal {self.name}- Image saved successfully")
