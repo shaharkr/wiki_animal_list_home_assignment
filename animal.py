542a6392cfc0d5dac222c1b9d119e0d16af7fae0
shaharkr <shahar.kramer@gmail.com>
2023-11-14 12:58:30 +0200

    animal class- add getters and setter

--- animal.py
+++ animal.py
@@ -7,11 +7,22 @@ class Animal:
     WIKI_PREFIX_URL = configurator.get_wiki_prefix()
     IMG_DIR_PATH = configurator.get_animals_img_dir_path()
     
-    def __init__(self, name: str, adjectives: List[str]) -> None:
-        self.name = name.replace(' ', '_')
-        self.wiki_page_url = Animal.WIKI_PREFIX_URL.format(link_extension=self.name)
-        self.img_path = f"{Animal.IMG_DIR_PATH}/{self.name}"
+    def __init__(self, name: str, adjectives: List[str], img_src: str = None) -> None:
+        self.name = name
+        self.wiki_page_url = Animal.WIKI_PREFIX_URL.format(link_extension=self.name.replace(' ', '_'))
+        self.img_path = f"{Animal.IMG_DIR_PATH}\{self.name.replace(' ', '_')}.jpg"
         self.adjectives = adjectives
+        self.img_src = img_src
     
     def __repr__(self) -> str:
-        return f"Name: {self.name}/n Wiki link: {self.wiki_page_url}"
+        return f"Name: {self.name}\n\t\tImage path link: {self.img_path}"
+    
+    def get_wiki_page_url(self):
+        return self.wiki_page_url
+    
+    def get_img_path(self):
+        return self.img_path
+    
+    def set_img_src(self, img_src: str) -> None:
+        self.img_src = img_src
+    
